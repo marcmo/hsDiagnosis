0f0ce8c719c82d31deabda97eeec6b427832eb78
oliver <oliver.mueller@gmail.com>
2011-03-07 09:54:49 +0100

    sample script file

--- Lua/script_send_diag.lua
+++ Lua/script_send_diag.lua
@@ -1,12 +1,19 @@
 require("base")
 
-setIp("10.40.39.19")
+local ip = "10.40.39.19"
+setIp(ip)
+--setIp("localhost")
 
 require("nvramtests")
-runNvramTests(2);
+--runNvramTests(2);
+
+--local msg=Diag.new{ 0x22,0x20,0x0 }
+--local resp = sendMsg(0xf4, 0x40, 2000, msg)
+--print("response was: " .. Diag.tostring(resp))
 
 
-
+showPrimaryDtcs(ip,0xf4, 0x40)
+showSecondaryDtcs(ip,0xf4, 0x40)
 print("here")
 logger:log(logging.WARN, "try to wait...")
 wait(200)
