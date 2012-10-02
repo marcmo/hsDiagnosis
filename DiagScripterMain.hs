bf15e7ee95dc174e468f0937fca0f5a39908cd94
oliver <oliver.mueller@gmail.com>
2012-10-02 08:12:15 +0200

    some cleanup

--- DiagScripterMain.hs
+++ DiagScripterMain.hs
@@ -2,14 +2,11 @@
 module Main where
 
 import System.Console.CmdArgs
-import qualified Com.DiagClient as DC (host ,DiagConfig(..))
-import Control.Monad (when)
+import qualified Com.DiagClient as DC (DiagConfig(..))
 import Network.Socket
 import Diagnoser.DiagScriptTester(runTestScript)
-import Config 
-import qualified Config as Conf
-import qualified Data.List as DL (intersperse)
-import Data.Maybe
+import Config
+import qualified Data.List as DL (intercalate)
 
 -- if a better help message and improved flags are required we better switch from CmdArgs to System.Console.GetOpt
 
@@ -26,48 +23,48 @@ data DiagExecuter = DiagExecuter {
 
 
 diagExecuter = DiagExecuter {
-                    script = def &= args, 
+                    script = def &= args,
                     config = defaultConfigFile &= typFile &= help ("configuration file, default points to \"" ++ defaultConfigFile ++ "\""),
                     ip     = def              &= help "ip address of ecu", -- what is an ecu
                     port   = def              &= help "ip address port",
                     source = def &= typ "Hex" &= help "default source, as hex e.g. f4",
                     target = def &= typ "Hex" &= help "default target, as hex e.g. 10",
                     verbose= def &= typ "Bool"&= help "verbose output",
-                    timeout= def &= typ "Int" &= help "timeout for responses of diagnosis messages" 
-                  } &= help "run diagnosis tests on a Diagnoser .skr file" 
+                    timeout= def &= typ "Int" &= help "timeout for responses of diagnosis messages"
+                  } &= help "run diagnosis tests on a Diagnoser .skr file"
 
 main ::  IO ()
-main = withSocketsDo $ do 
-  args <- cmdArgs $ diagExecuter
+main = withSocketsDo $ do
+  arguments <- cmdArgs $ diagExecuter
                       &= summary "DiagScripter 0.1.0, (c) Oliver Mueller 2010-2011"
-  fileConfig <-  diagConfigInFromFile (config args)
-  let argsConfig = DiagConfigIn (ip args) 
-                                (port args) 
-                                (source args >>= hexIt)
-                                (target args >>= hexIt)
-                                (verbose args) 
-                                (timeout args)
+  fileConfig <-  diagConfigInFromFile (config arguments)
+  let argsConfig = DiagConfigIn (ip arguments)
+                                (port arguments)
+                                (source arguments >>= hexIt)
+                                (target arguments >>= hexIt)
+                                (verbose arguments)
+                                (timeout arguments)
       finalConfig = mergeDiagConfigIns fileConfig argsConfig
-  executeOrError (script args) finalConfig
+  executeOrError (script arguments) finalConfig
 
 executeOrError :: FilePath -> DiagConfigIn -> IO ()
-executeOrError scriptPath c = 
+executeOrError scriptPath c =
   maybe (missingItemsError $ missingConfigItems c)
         (execute scriptPath)
         (inToDiagConfig c)
 
 missingConfigItems :: DiagConfigIn -> String
-missingConfigItems c@(DiagConfigIn ip port source target verbose timeout) = 
-   concat $ DL.intersperse ", "
-           ([] <: (ip, "ip") 
-               <: (port, "port") 
-               <: (source, "source") 
-               <: (target, "target") 
-               <: (verbose, "verbose") 
-               <: (timeout, "timeout"))
-  where xs <: (Nothing, name) = let x = "\"" ++ name ++ "\"" in 
-                                    xs ++ [x] 
-        xs <: (Just b, name ) = xs
+missingConfigItems (DiagConfigIn _ip _port _source _target _verbose _timeout) =
+   DL.intercalate ", "
+           ([] <: (_ip, "ip")
+               <: (_port, "port")
+               <: (_source, "source")
+               <: (_target, "target")
+               <: (_verbose, "verbose")
+               <: (_timeout, "timeout"))
+  where xs <: (Nothing, _name) = let x = "\"" ++ _name ++ "\"" in
+                                    xs ++ [x]
+        xs <: _ = xs
 
 missingItemsError :: String -> IO ()
 missingItemsError items = putStrLn $
@@ -75,6 +72,6 @@ missingItemsError items = putStrLn $
   "set them in the configuration file or via the command line flags"
 
 execute :: FilePath -> DC.DiagConfig -> IO ()
-execute f conf@(DC.MkDiagConfig ip _ _ _ _ _ ) = do
-    print $ "running script " ++ f ++ " on ip " ++ ip
+execute f conf@(DC.MkDiagConfig _ip _ _ _ _ _ ) = do
+    print $ "running script " ++ f ++ " on ip " ++ _ip
     runTestScript f conf
